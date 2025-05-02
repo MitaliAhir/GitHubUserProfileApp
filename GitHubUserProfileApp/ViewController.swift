b9722ad451f94a62f1ac2fffa1099570399c0c7f
Mitali Ahir <mitaliahir3@gmail.com>
2025-05-02 16:24:54 -0400

    Initial Commit

---
+++ GitHubUserProfileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubUserProfileApp
+//
+//  Created by Siddharth Trivedi on 2025-05-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
